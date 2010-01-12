0b1cca6e02f1a1eac4393be2585eeb45fcd172d4
attila.lendvai <attila.lendvai@gmail.com>
2010-01-12 13:37:42 +0000

    use defaults for logging

--- source/logger.lisp
+++ source/logger.lisp
@@ -6,7 +6,4 @@
 
 (in-package :hu.dwim.computed-class)
 
-(def logger computed-class ()
-  :runtime-level +warn+
-  :compile-time-level #+optimize +warn+ #-optimize +dribble+
-  :appender (make-instance 'brief-stream-appender :stream *debug-io*))
+(def logger computed-class ())
